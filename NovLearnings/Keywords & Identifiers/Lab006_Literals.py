dcfb43483c6eab26c3a40d3718a441caccd1ae20
srinivas.dasari <srinipega30@gmail.com>
2024-11-20 16:14:20 +0530

    Com

---
+++ NovLearnings/Keywords & Identifiers/Lab006_Literals.py
@@ -0,0 +1,15 @@
+
+
+
+num1 = float(input("Enter 1st number : "))
+num2 =  float(input("Enter 2nd number : "))
+
+Sum = (num1+num2)
+Diff = (num1-num2)
+Mul = (num1*num2)
+Div = (num1/num2)
+
+print ("The Sum of two numbers is : ",Sum)
+print ("The Difference of two numbers is :", Diff)
+print ("The Mulitply of two given numbers is : ", Mul)
+print ("The Division of two given numbers is : ", Div)
